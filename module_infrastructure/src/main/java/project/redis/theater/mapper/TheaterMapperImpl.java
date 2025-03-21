5dfb7e8b03710b714e90672052a62b347d24476d
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-21 14:12:25 +0900

    Refactor : 상영관 mapper 수정
    상영관 entity to domain mapper List 받아서 domain 생성하도록 수정

--- module_infrastructure/src/main/java/project/redis/theater/mapper/TheaterMapperImpl.java
+++ module_infrastructure/src/main/java/project/redis/theater/mapper/TheaterMapperImpl.java
@@ -17,7 +17,8 @@ public class TheaterMapperImpl implements TheaterMapper {
 
     @Override
     public Theater toDomain(TheaterEntity theaterEntity) {
-        return Theater.of(theaterEntity.getTheaterId(), theaterEntity.getTheaterName());
+        List<Seat> seats = convertSeatEntitiesToSeats(theaterEntity.getSeats());
+        return Theater.of(theaterEntity.getTheaterId(), theaterEntity.getTheaterName(), seats);
     }
 
     public List<Seat> convertSeatEntitiesToSeats(List<SeatEntity> seatEntities) {
@@ -25,30 +26,4 @@ public class TheaterMapperImpl implements TheaterMapper {
                 .map(seatMapper::toDomain)
                 .toList();
     }
-
-    // CineamSeats를 Cinema 생성할 때마다 자동 생성하게 하니 Entity에서 domain 클래스로 변환할 때 어떻게 해야 할지 모르겠음
-    /*
-    public CinemaSeats convertSeatsToCinemaSeats(List<Seat> seats) {
-        List<Seat> sortedSeats = sortSeats(seats);
-
-        List<List<Seat>> cinemaSeats = new ArrayList<>();
-
-        String startRow = sortedSeats.get(0).getSeatRow();
-        int index = 0;
-        return null;
-    }
-
-    private List<Seat> sortSeats(List<Seat> seats) {
-        seats.sort((seat1, seat2) -> {
-            // row 비교
-            int rowComparison = seat1.compareRow(seat2);
-            if (rowComparison != 0) {
-                return rowComparison;
-            }
-            // col 비교
-            return seat1.compareCol(seat2);
-        });
-        return seats;
-    }
-     */
 }
