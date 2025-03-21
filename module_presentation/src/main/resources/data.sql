b0642856b5c0e1f1126b6aeeba8b21940aff409c
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-22 01:25:03 +0900

    Chore : application.yml 수정
    application.yml에 sql 부분을 위로 올림
    data.sql 적용하기 위해서는 datasource와 jpa 사이에 있어야 함

--- module_presentation/src/main/resources/data.sql
+++ module_presentation/src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO cinema (cinema_name, created_by, created_at, updated_by, updated_at)
-VALUES
+insert into cinema (cinema_name, created_by, created_at, updated_by, updated_at)
+values
     ('Cinema 1', null, null, null, null),
     ('Cinema 2', null, null, null, null),
     ('Cinema 3', null, null, null, null),
     ('Cinema 4', null, null, null, null),
     ('Cinema 5', null, null, null, null);
 
-INSERT INTO theater (theater_name, cinema_id, created_by, created_at, updated_by, updated_at)
-VALUES
+insert into theater (theater_name, cinema_id, created_by, created_at, updated_by, updated_at)
+values
     ('Theater a1', 1, null, null, null, null),
     ('Theater a2', 1, null, null, null, null),
     ('Theater b1', 2, null, null, null, null),
@@ -19,8 +19,8 @@ VALUES
     ('Theater e1', 5, null, null, null, null),
     ('Theater e2', 5, null, null, null, null);
 
-INSERT INTO movie (title, rating, released_at, thumbnail, duration, genre, created_by, created_at, updated_by, updated_at)
-VALUES
+insert into movie (title, rating, released_at, thumbnail, duration, genre, created_by, created_at, updated_by, updated_at)
+values
     ('Movie 1', 'ALL', '2025-03-01', 'https://thumbnail1.jpg', 120, 'ACTION', null, null, null, null),
     ('Movie 2', 'TWELVE', '2025-03-05', 'https://thumbnail2.jpg', 90, 'ROMANCE', null, null, null, null),
     ('Movie 3', 'FIFTEEN', '2025-03-10', 'https://thumbnail3.jpg', 105, 'ACTION', null, null, null, null),
@@ -33,8 +33,8 @@ VALUES
     ('Movie 10', 'RESTRICT', '2025-03-19', 'https://thumbnai20.jpg', 105, 'HORROR', null, null, null, null);
 
 
-INSERT INTO screening (started_at, ended_at, movie_id, theater_id, created_by, created_at, updated_by, updated_at)
-VALUES
+insert into screening (started_at, ended_at, movie_id, theater_id, created_by, created_at, updated_by, updated_at)
+values
     ('2025-03-21 10:00:00', '2025-03-21 12:00:00', 1, 1, null, null, null, null),
     ('2025-03-21 10:00:00', '2025-03-21 12:00:00', 1, 1, null, null, null, null),
     ('2025-03-21 10:00:00', '2025-03-21 12:00:00', 1, 1, null, null, null, null),
@@ -146,15 +146,15 @@ VALUES
     ('2025-03-21 10:00:00', '2025-03-21 12:00:00', 10, 10, null, null, null, null);
 
 
-INSERT INTO seat (is_reserved, seat_row, seat_column, theater_id, created_by, created_at, updated_by, updated_at)
-VALUES
-    (FALSE, 'A', 1, 1, null, null, null, null),
-    (FALSE, 'A', 1, 2, null, null, null, null),
-    (FALSE, 'A', 1, 3, null, null, null, null),
-    (FALSE, 'A', 1, 4, null, null, null, null),
-    (FALSE, 'A', 1, 5, null, null, null, null),
-    (FALSE, 'A', 1, 6, null, null, null, null),
-    (FALSE, 'A', 1, 7, null, null, null, null),
-    (FALSE, 'A', 1, 8, null, null, null, null),
-    (FALSE, 'A', 1, 9, null, null, null, null),
-    (FALSE, 'A', 1, 10, null, null, null, null);
+insert into seat (is_reserved, seat_row, seat_column, theater_id, created_by, created_at, updated_by, updated_at)
+values
+    (0, 'A', 1, 1, null, null, null, null),
+    (0, 'A', 1, 2, null, null, null, null),
+    (0, 'A', 1, 3, null, null, null, null),
+    (0, 'A', 1, 4, null, null, null, null),
+    (0, 'A', 1, 5, null, null, null, null),
+    (0, 'A', 1, 6, null, null, null, null),
+    (0, 'A', 1, 7, null, null, null, null),
+    (0, 'A', 1, 8, null, null, null, null),
+    (0, 'A', 1, 9, null, null, null, null),
+    (0, 'A', 1, 10, null, null, null, null);
