245f12f40907792edf68726291687a533cec1906
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-21 13:43:19 +0900

    Refactor : 상영관 좌석 클래스 리팩토링
    상영관 좌석들 미리 생성하는 것만 있었음
    여기에 좌석 리스트 받아서 생성하는 것도 추가
    왜냐하면 entity to domain에서 어려움이 있었음

--- module_domain/src/main/java/project/redis/theater/Theater.java
+++ module_domain/src/main/java/project/redis/theater/Theater.java
@@ -1,8 +1,10 @@
 package project.redis.theater;
 
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import project.redis.seat.Seat;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Theater {
@@ -13,5 +15,9 @@ public class Theater {
 
     public static Theater of(Long theaterId, String theaterName) {
         return new Theater(theaterId, theaterName, TheaterSeats.create());
+    }
+
+    public static Theater of(Long theaterId, String theaterName, List<Seat> seats) {
+        return new Theater(theaterId, theaterName, TheaterSeats.create(seats));
     }
 }
--- module_domain/src/main/java/project/redis/theater/TheaterSeats.java
+++ module_domain/src/main/java/project/redis/theater/TheaterSeats.java
@@ -1,44 +1,45 @@
 package project.redis.theater;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import project.redis.seat.Seat;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TheaterSeats {
 
     public static final int ROWS = 5;
     public static final int COLUMNS = 5;
 
-    private final List<List<Seat>> seats;
+    private final List<Seat> seats;
 
-    public static TheaterSeats create() {
-        return new TheaterSeats(createSeats());
-    }
-
-    private static List<List<Seat>> createSeats() {
-        List<List<Seat>> seats = new ArrayList<>();
+    private TheaterSeats() {
+        seats = new ArrayList<>();
         for (int row = 0; row < ROWS; row++) {
-            List<Seat> seat = new ArrayList<>();
             for (int col = 0; col < COLUMNS; col++) {
                 Seat oneSeat = createOneSeat(row, col);
-                seat.add(oneSeat);
+                seats.add(oneSeat);
             }
-            seats.add(Collections.unmodifiableList(seat));
         }
-        return Collections.unmodifiableList(seats);
     }
 
-    private static Seat createOneSeat(int row, int col) {
+    private TheaterSeats(List<Seat> seats) {
+        this.seats = new ArrayList<>(seats);
+    }
+
+    public static TheaterSeats create() {
+        return new TheaterSeats();
+    }
+
+    public static TheaterSeats create(List<Seat> seats) {
+        return new TheaterSeats(seats);
+    }
+
+    private Seat createOneSeat(int row, int col) {
         String seatRow = getSeatRow(row);
         Integer setColumn = col + 1;
         return Seat.of(null, false, seatRow, setColumn);
     }
 
-    private static String getSeatRow(int rowNumber) {
+    private String getSeatRow(int rowNumber) {
         return String.valueOf((char) ('A' + rowNumber));
     }
 }
