4c4edb3c1635ce7a5a181731bb88ff928a845afb
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-23 23:01:33 +0900

    Refactor : MovieQueryService 리팩토링
    MovieQueryService 메서드 추출해서 리팩토링
    메서드명은 조금 더 수정해야 할 것 같음

--- module_application/src/main/java/project/redis/movie/service/MovieQueryService.java
+++ module_application/src/main/java/project/redis/movie/service/MovieQueryService.java
@@ -23,43 +23,58 @@ public class MovieQueryService {
     // @Cacheable(cacheNames = "movieCache")
     @Cacheable(cacheNames = "redisCache")
     public List<NowPlayMovieDto> getNowPlayingMovies(String movieTitle, String movieGenre) {
+        MovieGenre movieGenreEnum = getMovieGenre(movieGenre);
+
+        List<ScreeningResponseDto> moviesGroupedByTheater
+                = screeningRepository.findMoviesGroupedByTheater(movieTitle, movieGenreEnum, LocalDate.now());
+
+        Map<String, Map<String, List<ScreeningResponseDto>>> groupedByMovieAndTheater
+                = mapMovieInfoByMovieAndTheater(moviesGroupedByTheater);
+
+        return extractNowPlayMovieDtos(groupedByMovieAndTheater);
+    }
+
+    private MovieGenre getMovieGenre(String movieGenre) {
         MovieGenre movieGenreEnum = null;
 
         if (movieGenre != null) {
             movieGenreEnum = MovieGenre.valueOf(movieGenre);
         }
+        return movieGenreEnum;
+    }
 
-        List<ScreeningResponseDto> moviesGroupedByTheater
-                = screeningRepository.findMoviesGroupedByTheater(movieTitle, movieGenreEnum, LocalDate.now());
+    private Map<String, Map<String, List<ScreeningResponseDto>>> mapMovieInfoByMovieAndTheater(
+            List<ScreeningResponseDto> moviesGroupedByTheater) {
+        return moviesGroupedByTheater.stream()
+                .collect(Collectors.groupingBy(ScreeningResponseDto::getMovieTitle,
+                        Collectors.groupingBy(ScreeningResponseDto::getCinemaAndTheaterName)));
+    }
 
-        Map<String, Map<String, List<ScreeningResponseDto>>> groupedByMovieAndTheater = moviesGroupedByTheater.stream()
-                .collect(Collectors.groupingBy(
-                        ScreeningResponseDto::getMovieTitle,  // 첫 번째 그룹: movieTitle 기준
-                        Collectors.groupingBy(
-                                ScreeningResponseDto::getCinemaAndTheaterName))); // 두 번째 그룹: cinemaAndTheaterName 기준
-
-        // Flatten 리스트
+    private List<NowPlayMovieDto> extractNowPlayMovieDtos(
+            Map<String, Map<String, List<ScreeningResponseDto>>> groupedByMovieAndTheater) {
         return groupedByMovieAndTheater.values().stream()
-                .map(stringListMap -> {
-                    // 영화의 상영 리스트를 변환
-                    return stringListMap.values().stream()
-                            .map(screenings -> {
-                                // 영화의 각 극장 및 상영 정보
-
-                                // ScreeningTimeDto 변환
-                                List<ScreeningTimeDto> screeningTimeDtos = screenings.stream()
-                                        .map(ScreeningTimeDto::createByQueryDto)
-                                        .sorted(Comparator.comparing(ScreeningTimeDto::getStartTime))
-                                        .collect(Collectors.toList());
-
-                                // NowPlayMovieDto 빌더 패턴을 사용하여 DTO 생성
-                                ScreeningResponseDto screeningResponseDto = screenings.get(0);
-                                return NowPlayMovieDto.createByQueryDto(screeningResponseDto, screeningTimeDtos);
-                            })
-                            .collect(Collectors.toList());
-                })
+                .map(this::convertToNowPlayMovieDtos)
                 .flatMap(List::stream)
                 .sorted(Comparator.comparing(NowPlayMovieDto::getMovieReleaseDate).reversed())
+                .collect(Collectors.toList());
+    }
+
+    private List<NowPlayMovieDto> convertToNowPlayMovieDtos(Map<String, List<ScreeningResponseDto>> stringListMap) {
+        return stringListMap.values().stream()
+                .map(this::createNowPlayMovieDto)
+                .collect(Collectors.toList());
+    }
+
+    private NowPlayMovieDto createNowPlayMovieDto(List<ScreeningResponseDto> screenings) {
+        List<ScreeningTimeDto> screeningTimeDtos = convertToScreeningTimeDtos(screenings);
+        ScreeningResponseDto screeningResponseDto = screenings.get(0);
+        return NowPlayMovieDto.createByQueryDto(screeningResponseDto, screeningTimeDtos);
+    }
+
+    private List<ScreeningTimeDto> convertToScreeningTimeDtos(List<ScreeningResponseDto> screenings) {
+        return screenings.stream()
+                .map(ScreeningTimeDto::createByQueryDto)
+                .sorted(Comparator.comparing(ScreeningTimeDto::getStartTime))
                 .collect(Collectors.toList());
     }
 }
