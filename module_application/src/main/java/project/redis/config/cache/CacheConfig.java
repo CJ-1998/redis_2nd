082274ffc4bdf3fbf91df970769c7a1e3cf84d1c
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-23 14:10:25 +0900

    Chore : 로컬 캐시 관련 설정 구현
    application 모듈의 config 폴더에 로컬 캐시 관련 설정 구현

---
+++ module_application/src/main/java/project/redis/config/cache/CacheConfig.java
@@ -0,0 +1,35 @@
+package project.redis.config.cache;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        List<CaffeineCache> caches = Arrays.stream(CacheType.values())
+                .map(cache -> new CaffeineCache(
+                                cache.getCacheName(),
+                                Caffeine.newBuilder()
+                                        .expireAfterWrite(cache.getExpireTimeInSeconds(), TimeUnit.SECONDS)
+                                        .maximumSize(cache.getMaxCachingSize())
+                                        .recordStats()
+                                        .build()
+                        )
+                )
+                .toList();
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+}
